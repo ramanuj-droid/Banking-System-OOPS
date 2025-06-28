7ec7d900b30c51878b4cc029bc7408194cfd6f70
ramanuj-droid <sage.rama.pb@gmail.com>
2025-06-28 15:08:42 +0530

    Update transaction.py
    log transaction

--- transaction.py
+++ transaction.py
@@ -17,3 +17,14 @@ class TransactionManager:
         else:
             self.account.withdraw(amount)
             return f"✅ ₹{amount} withdrawn successfully."
+def log_transaction(user, txn_type, amount, balance, session_state):
+    import datetime
+    txn = {
+        "Date/Time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "Type": txn_type,
+        "Amount": amount,
+        "Balance": balance
+    }
+    if user not in session_state["transactions"]:
+        session_state["transactions"][user] = []
+    session_state["transactions"][user].append(txn)
